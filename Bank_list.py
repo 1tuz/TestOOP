09acc0078fa2a50594e83c2d7782214f48e99959
Anton Yachmenyov <creameanwonder@gmail.com>
2022-04-10 22:31:00 +0300

    Вызов параметров различных банков.

--- Bank_list.py
+++
@@ -1,7 +0,0 @@
-from Bank_params import Banks
-
-rncb = Banks("RNCB", "Crimea", "commercial")
-tinkof = Banks("Tinkoff", "Europe", "commercial")
-sber = Banks("Sberbank", "Russia", "national")
-
-print(rncb.name, rncb.adress)
--- Bank_params.py
+++ Bank_params.py
@@ -1,8 +1,14 @@
-import Bank_list
-
-
 class Banks:
     def __init__(self, name, adress, btype):
         self.name = name
         self.adress = adress
         self.btype = btype
+
+
+rncb = Banks("RNCB", "Crimea", "commercial")
+tinkof = Banks("Tinkoff", "Europe", "commercial")
+sber = Banks("Sberbank", "Russia", "national")
+
+print(rncb.name)
+print(sber.adress)
+print(tinkof.name,tinkof.adress,tinkof.btype)
