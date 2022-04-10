8fc88440a08a614400c212db03f74ef3a2b7aaa6
Anton Yachmenyov <creameanwonder@gmail.com>
2022-04-10 21:19:06 +0300

    Создал класс банка и добавил несколько банков

---
+++ Bank_list.py
@@ -0,0 +1,6 @@
+import Bank_params
+class Banks:
+    def __init__(self,name,adress,type):
+        self.name = name
+        self.adress = adress
+        self.type = type
---
+++ Bank_params.py
@@ -0,0 +1,5 @@
+from Bank_list import Banks
+
+rncb = Banks("RNCB", "Crimea", "commercial")
+tinkof = Banks("Tinkoff","Europe","commercial")
+sber = Banks("Sberbank","Russia","national")
