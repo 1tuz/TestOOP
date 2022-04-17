1cbf064da302d07c9a2b2013ebb3efb462ea543c
Anton Yachmenyov <creameanwonder@gmail.com>
2022-04-17 17:07:22 +0300

    Easy API class adding

--- API_testing.py
+++ API_testing.py
@@ -2,6 +2,15 @@ import requests
 import pytest
 
 
-response = requests.get("https://yandex.ru/")
+class EasyAPI:
+    r = requests.get("https://yandex.ru/")
+    if r.status_code == 200:
+        print(r.status_code, "Ok")
+    else:
+        print("Warning! Response status code = ", r.status_code)
 
-print(response.text)
+    w = requests.get("https://yandex.ru/testtt")
+    if w.status_code == 200:
+        print(w.status_code, "OK")
+    else:
+        print("Warning! Response status code = ", w.status_code)
