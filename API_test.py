5bce6660447d50a2ec5280713d8b038af2d790d9
Anton Yachmenyov <creameanwonder@gmail.com>
2022-04-17 20:04:40 +0300

    Added asserting

--- API_test.py
+++ API_test.py
@@ -2,15 +2,19 @@ import requests
 import pytest
 
 
-class EasyAPI:
-    r = requests.get("https://yandex.ru/")
+class TestEasyAPI:
+    r = requests.get("https://github.com/1tuz/TestOOP")
     if r.status_code == 200:
         print(r.status_code, "Ok")
     else:
         print("Warning! Response status code = ", r.status_code)
 
-    w = requests.get("https://yandex.ru/testtt")
+    w = requests.get("https://github.com/1tuz/TestOOP/11111")
     if w.status_code == 200:
         print(w.status_code, "OK")
     else:
         print("Warning! Response status code = ", w.status_code)
+
+    def test_github_assertion(self):
+        y = requests.get("https://github.com/1tuz/TestOOP/11111")
+        assert y.status_code == 200
